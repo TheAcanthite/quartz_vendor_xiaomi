50433a3d7888dc2f6536e084494b1c08f46da5ca
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-04 20:42:15 +0200

    mido: Remove prebuilt libbtconfigstore

--- mido/mido-vendor.mk
+++ mido/mido-vendor.mk
@@ -1171,7 +1171,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mido/proprietary/vendor/radio/qcril_database/qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db
 
 PRODUCT_PACKAGES += \
-    libbtconfigstore \
     libgpustats \
     libsdm-disp-vndapis \
     libthermalclient \
